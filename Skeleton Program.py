4594376a1613dc09abcb1c7c1538403a26d69ce0
thetomhoward97 <whitehartlane.24@hotmail.co.uk>
2014-04-03 14:25:47 +0100

    Task 3(b) complete
    Give user option to add score to table

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -177,21 +177,33 @@ def DisplayRecentScores(RecentScores):
   print()
 
 def UpdateRecentScores(RecentScores, Score):
-  PlayerName = GetPlayerName()
-  FoundSpace = False
-  Count = 1
-  while (not FoundSpace) and (Count <= NO_OF_RECENT_SCORES):
-    if RecentScores[Count].Name == '':
-      FoundSpace = True
-    else:
-      Count = Count + 1
-  if not FoundSpace:
-    for Count in range(1, NO_OF_RECENT_SCORES):
-      RecentScores[Count].Name = RecentScores[Count + 1].Name
-      RecentScores[Count].Score = RecentScores[Count + 1].Score
-    Count = NO_OF_RECENT_SCORES
-  RecentScores[Count].Name = PlayerName
-  RecentScores[Count].Score = Score
+  ValidInput=False
+  while ValidInput==False:
+    UpdateScore=input('Do you want to add your name to the recent score table? (y or n): ').lower()
+    print('')
+    if UpdateScore == 'yes':
+      UpdateScore='y'
+    elif UpdateScore == 'no':
+      UpdateScore='n'
+    if UpdateScore=='n':
+      ValidInput=True
+    elif UpdateScore=='y':
+      PlayerName = GetPlayerName()
+      FoundSpace = False
+      Count = 1
+      while (not FoundSpace) and (Count <= NO_OF_RECENT_SCORES):
+        if RecentScores[Count].Name == '':
+          FoundSpace = True
+        else:
+          Count = Count + 1
+      if not FoundSpace:
+        for Count in range(1, NO_OF_RECENT_SCORES):
+          RecentScores[Count].Name = RecentScores[Count + 1].Name
+          RecentScores[Count].Score = RecentScores[Count + 1].Score
+        Count = NO_OF_RECENT_SCORES
+      RecentScores[Count].Name = PlayerName
+      RecentScores[Count].Score = Score
+      ValidInput=True
 
 def PlayGame(Deck, RecentScores):
   LastCard = TCard()
---
+++ Task3(b).md
@@ -0,0 +1,3 @@
+#Task 3 part B
+##Question 1
+The funcion responsible for adding the scores to the table is UpdateRecentScores(RecentScores, Score)
